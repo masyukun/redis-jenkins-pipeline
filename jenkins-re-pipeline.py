8cd2dab897d5e9537e20063707e884ea2764a9b3
Matthew Royal <matthew.royal@redislabs.com>
2021-09-10 10:44:23 -0700

    Initial commit

---
+++ jenkins-re-pipeline.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import json
+import requests # REST requests
+import click    # command-line argument parsing
+
+
+
+def getfile(filename):
+    f = open(filename,)
+    fileobject = json.load(f)
+    f.close()
+    return fileobject
+
+
+@click.command()
+@click.option('--deployfile', help='Path to the deployment configuration file.')
+def runJenkinsPipeline(deployfile):
+    """Jenkins pipeline script for creating databases."""
+
+    # Get environment variables
+    REDIS_SERVER_FQDN = os.environ.get('REDIS_SERVER_FQDN')
+    REDIS_SERVER_PORT = os.environ.get('REDIS_SERVER_PORT')
+    REDIS_USER = os.environ.get('REDIS_USER')
+    REDIS_PASS = os.environ.get('REDIS_PASS')
+
+
+    # Get the deployment config file from the user
+    deployconfig = getfile(deployfile)
+    print (deployconfig["database"]["name"])
+
+    # Get pre-made t-shirt file
+    dbsize = deployconfig["database"]["size"]
+    tshirtfile = getfile("./redis-standard-size-"+dbsize+".json.template")
+    print (tshirtfile)
+
+    # Customize the t-shirt file with user specs
+    tshirtfile["name"] = deployconfig["database"]["name"]
+    print (tshirtfile)
+
+    # Create the database
+    url = "https://" + REDIS_SERVER_FQDN + ":" + REDIS_SERVER_PORT + "/v1/bdbs"
+    print (url)
+    #response = requests.post(url, json=tshirtfile)
+    #print (response.json())
+
+
+
+
+if __name__ == "__main__":
+    runJenkinsPipeline()
+    
+    
